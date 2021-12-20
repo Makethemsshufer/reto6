fe8901a1282ab1d80000dd805a533ff944d08675
Manuel Garces <91733802+Makethemsshufer@users.noreply.github.com>
2021-12-19 23:25:49 -0500

    Update ProductController.java

--- src/main/java/Reto4Ciclo4/controller/ProductController.java
+++ src/main/java/Reto4Ciclo4/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/cosmetics")
+@RequestMapping("/api/hairproducts")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ProductController {
     @Autowired
